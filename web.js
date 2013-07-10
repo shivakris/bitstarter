e55967c7807e584e03b9c1c1a3f6e16e0fa8fa21
= <shivakris@gmail.com>
2013-07-10 21:53:04 +0000

    added

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var fs = require('fs');
 
 app.get('/', function(request, response) {
-  var buf = new Buffer(fs.readfile(index.html));
+  var buf = new Buffer(fs.readfile('index.html'));
   response.send(buf.toString());
 });
 
